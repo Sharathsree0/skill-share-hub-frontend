f423e167c02f8d1e88af508800107e4dfdc11a1d
ANJAD KT <156407030+anjadkt@users.noreply.github.com>
2026-03-03 14:28:57 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,24 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "../features/auth/Login";
+import { useEffect } from "react"
+import { checkAuth } from "../features/profile/userSlice";
+import { useAppDispatch } from "../shared/hooks/redux";
+import { Route,Routes } from "react-router";
+import AuthLayout from "../layouts/AuthLayout";
 import Register from "../features/auth/Register";
+import Login from "../features/auth/Login";
 import ProfileSetup from "../features/profile/profileSetup";
 import Dashboard from "../features/dashboard/Dashboard";
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(()=>{
+    dispatch(checkAuth());
+  },[dispatch])
   return (
     <Routes>
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route element={<AuthLayout/>}>
+       <Route path="/login" element={<Login/>} />
+       <Route path="/register" element={<Register/>} />
+      </Route>
       <Route path="/profile" element={<ProfileSetup /> }/>
       <Route path="/dashboard" element={<Dashboard/> }/>
     </Routes>
