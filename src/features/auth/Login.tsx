a67e8c7851774a2ba57b971232074cf0b36fb157
anjadkt <anjad076@gmail.com>
2026-03-03 11:53:46 +0530

    add forgot pass phase 1

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -10,6 +10,8 @@ import type {LoginInput} from './authTypes'
 import { type LogFormData, logSchema } from "./validations";
 import { GoogleLogin } from "@react-oauth/google";
 import { handleOauth } from "./service";
+import {z} from 'zod'
+import Forgot from "./components/Forget";
 
 export default function Login() {
 
@@ -19,6 +21,7 @@ export default function Login() {
   });
   const [error, setError] = useState<Partial<LogFormData>>({});
   const [loading,setLoading] = useState(false);
+  const [open,setOpen] = useState(false);
 
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
@@ -60,6 +63,28 @@ export default function Login() {
     }
   }
 
+  const handleForget = async () => {
+    const emailSchema = z.string().min(1,"Email Required!").email("Enter a valid email!");
+    const result = emailSchema.safeParse(form.email);
+    if(!result.success){
+      setError({email : result.error.issues[0].message});
+      return;
+    }
+    try{
+      // const {data} = await api.post("auth/forgot-otp",form);
+      // if(data.success){
+      //   setOpen(true);
+      // }
+    }catch(error){
+      if(axios.isAxiosError(error)){
+        switch(error.response?.status){
+
+        }
+      }
+    }
+    setOpen(true);
+  }
+
   const inputArray: LoginInput[] = [
     { name: "email", label: "Email Address", type: "email" },
     { name: "password", label: "Password", type: "password" }
@@ -68,62 +93,68 @@ export default function Login() {
   return (
     <div className="flex h-[520px] w-full max-w-4xl overflow-hidden rounded-2xl bg-white shadow-2xl">
 
-        <div className="flex w-full flex-col justify-center p-8 md:w-1/2 lg:p-12">
-          <div className="mb-6 md:hidden">
-            <h1 className="text-2xl font-bold text-gray-800">Login Now</h1>
-          </div>
+        {
+          open ? (<Forgot/>) :
+          (
+            <div className="flex w-full flex-col justify-center p-8 md:w-1/2 lg:p-12">
+              <div className="mb-6 md:hidden">
+                <h1 className="text-2xl font-bold text-gray-800">Login Now</h1>
+              </div>
 
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            {inputArray.map((v, i) => (
-              <Input
-                key={i}
-                error={error[v.name] || null}
-                label={v.label}
-                name={v.name}
-                onChange={handleChange}
-                value={form[v.name]}
-                type={v.type}
-              />
-            ))}
+              <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                {inputArray.map((v, i) => (
+                  <Input
+                    key={i}
+                    error={error[v.name] || null}
+                    label={v.label}
+                    name={v.name}
+                    onChange={handleChange}
+                    value={form[v.name]}
+                    type={v.type}
+                  />
+                ))}
 
-            <div className="flex items-center justify-between text-sm px-1">
-              <label className="flex items-center gap-2 cursor-pointer text-gray-600">
-                <input 
-                  type="checkbox" 
-                  className="w-4 h-4 rounded border-gray-300 accent-[#145537] cursor-pointer" 
-                />
-                <span>Remember me</span>
-              </label>
-              <button 
-                type="button"
-                className="font-medium text-[#145537] hover:underline cursor-pointer"
-              >
-                Forgot password?
-              </button>
+                <div className="flex items-center justify-between text-sm px-1">
+                  <label className="flex items-center gap-2 cursor-pointer text-gray-600">
+                    <input 
+                      type="checkbox" 
+                      className="w-4 h-4 rounded border-gray-300 accent-[#145537] cursor-pointer" 
+                    />
+                    <span>Remember me</span>
+                  </label>
+                  <button 
+                    type="button"
+                    onClick={handleForget}
+                    className="font-medium text-[#145537] hover:underline cursor-pointer"
+                  >
+                    Forgot password?
+                  </button>
+                </div>
+                
+                <button
+                  type="submit"
+                  className="mt-2 w-full rounded-lg bg-[#145537] py-3 font-semibold text-white transition hover:bg-[#0d3e27] cursor-pointer active:scale-95"
+                >
+                  {loading?(<ButtonSpinner/>):"Sign in"}
+                </button>
+              </form>
+
+              <div className="relative my-6 text-center">
+                <hr className="border-gray-200" />
+                <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-xs font-medium text-gray-400">
+                  OR
+                </span>
+              </div>
+
+              <GoogleLogin shape="pill" onSuccess={handleOauth} onError={()=>console.log("Login failed")} />
+
+              <div className="mt-6 text-sm text-gray-500 flex justify-center items-center gap-1">
+                <div>Don't have an account?</div>
+                <button onClick={()=>navigate('/register')} className="font-bold cursor-pointer text-[#145537] hover:underline">Sign up</button>
+              </div>
             </div>
-            
-            <button
-              type="submit"
-              className="mt-2 w-full rounded-lg bg-[#145537] py-3 font-semibold text-white transition hover:bg-[#0d3e27] cursor-pointer active:scale-95"
-            >
-              {loading?(<ButtonSpinner/>):"Sign in"}
-            </button>
-          </form>
-
-          <div className="relative my-6 text-center">
-            <hr className="border-gray-200" />
-            <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-xs font-medium text-gray-400">
-              OR
-            </span>
-          </div>
-
-          <GoogleLogin shape="pill" onSuccess={handleOauth} onError={()=>console.log("Login failed")} />
-
-          <div className="mt-6 text-sm text-gray-500 flex justify-center items-center gap-1">
-            <div>Don't have an account?</div>
-            <button onClick={()=>navigate('/register')} className="font-bold cursor-pointer text-[#145537] hover:underline">Sign up</button>
-          </div>
-        </div>
+          )
+        }
 
         <div className="hidden w-1/2 flex-col justify-center bg-[#145537] p-12 text-white md:flex">
           <div className="mb-8 flex items-center gap-3">
